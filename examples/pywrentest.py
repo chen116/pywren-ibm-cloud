a3e576c59aa6a15c158e3fb5ee97286bcc2fa973
Victor Chen <yvictorck@gmail.com>
2019-01-07 15:25:33 -0800

    a

--- examples/pywrentest.py
+++ examples/pywrentest.py
@@ -8,6 +8,6 @@ def my_function(x):
     return x + 7
 
 if __name__ == '__main__':
-    pw = pywren.ibm_cf_executor()
+    pw = pywren.ibm_cf_executor(logging='DEBUG')
     pw.call_async(my_function, 3)
     print (pw.get_result())
