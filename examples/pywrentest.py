e8a42e7f06aec499f61306593fc0324dddadd080
Victor Chen <yvictorck@gmail.com>
2019-01-08 18:36:46 -0800

    a

--- examples/pywrentest.py
+++ examples/pywrentest.py
@@ -9,5 +9,6 @@ def my_function(x):
 
 if __name__ == '__main__':
     pw = pywren.ibm_cf_executor(log_level='DEBUG')
+    print(pw.is_cf_cluster)
     pw.call_async(my_function, 3)
     print (pw.get_result())
