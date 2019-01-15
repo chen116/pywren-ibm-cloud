d052ba367da108a93652a4f7ad9746d93087b8c0
Victor Chen <yvictorck@gmail.com>
2019-01-15 14:37:16 -0800

    a

--- examples/pywrentest.py
+++ examples/pywrentest.py
@@ -4,6 +4,21 @@ Simple PyWren example using one single function invocation
 import pywren_ibm_cloud as pywren
 
 
+
+my_source = my_source(socket,window=5)
+my_sink = my_sink(sth)
+
+def my_func(x):
+	return sum(x)
+pw = pywren.ibm_cf_executor()
+pw.streamprocess(my_func,my_sink)
+
+
+
+
+
+
+
 def my_function(x):
     return x + 7
 
