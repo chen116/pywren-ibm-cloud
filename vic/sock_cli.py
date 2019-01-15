5ea8843eb897aefa399e4466945b5455ab211a27
Victor Chen <yvictorck@gmail.com>
2019-01-15 14:42:58 -0800

    a

--- vic/sock_cli.py
+++ vic/sock_cli.py
@@ -4,8 +4,10 @@ HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	i=input()
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-
-print('Received', repr(data))
+		while i!='q':
+    		s.sendall(b'Hello, world')
+    		data = s.recv(1024)
+			print('Received', repr(data))
+			i=input()
