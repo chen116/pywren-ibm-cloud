560fc54a4513a3497c6d62b7a752d2718264477c
Victor Chen <yvictorck@gmail.com>
2019-01-15 15:01:51 -0800

    a

--- vic/sock_serv.py
+++ vic/sock_serv.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
@@ -7,10 +8,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
+    i=1
     with conn:
         print('Connected by', addr)
         while True:
-            data = conn.recv(1024)
+            time.sleep(1)
+            # data = conn.recv(1024)
             if not data:
                 break
-            conn.sendall(data)
+            conn.sendall(i)
+            i+=1
