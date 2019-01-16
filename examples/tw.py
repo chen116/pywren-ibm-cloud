7bc7af2f1eaa42bb7540306324cb3ad2f74ea291
Victor Chen <yvictorck@gmail.com>
2019-01-16 00:59:57 -0800

    a

--- examples/tw.py
+++ examples/tw.py
@@ -79,7 +79,8 @@ def streamprocess_threads(pw,my_func,my_reduce_function,connector,host='localhos
             if self.cnt>=5:
                 exitFlag=1 
                 self.on_error(420)
-                # sys.exit('Limit tweets reached.')
+                print(pw.get_result())
+                sys.exit('Limit tweets reached.')
 
 
 
