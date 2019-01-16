bf039ea8a16439d1e161082af94925a79e31d5b6
Victor Chen <yvictorck@gmail.com>
2019-01-15 19:01:04 -0800

    a

--- vic/sock_cli.py
+++ vic/sock_cli.py
@@ -14,7 +14,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         try:
             data = s.recv(4)
-        except socket.error, e:
+        except socket.error as e:
             err = e.args[0]
             if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
                 print('No data available')
