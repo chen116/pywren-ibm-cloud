3b905588a21fd6efdf67e768cf8233c0863d2192
Victor Chen <yvictorck@gmail.com>
2019-01-15 20:46:09 -0800

    a

--- examples/pywrentest.py
+++ examples/pywrentest.py
@@ -4,23 +4,81 @@ Simple PyWren example using one single function invocation
 import pywren_ibm_cloud as pywren
 import socket,fcntl,os,errno
 import time
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+import Queue
+import threading
 
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.connect((HOST, PORT))
-#     # s.sendall(b"Hello, world")
-#     while True:
-#         data = s.recv(4)
-#         if data:
-#             print("Received", repr(data))
-#         else:
-#             print('meow')
-#             break
-#     s.close()
-#     print('closed')
-# my_source = my_source(socket,window=5)
 
+def streamprocess_threads(pw,my_func,my_reduce_function,connector='socket',host='localhost',port=65432,window=2):
+    
+    exitFlag = 0
+    class myThread (threading.Thread):
+        def __init__(self, threadID, name, q):
+            threading.Thread.__init__(self)
+            self.threadID = threadID
+            self.name = name
+            self.q = q
+        def run(self):
+            print "Starting " + self.name
+            while not exitFlag:
+                queueLock.acquire()
+                if not workQueue.empty():
+                    data = q.get()
+                    queueLock.release()
+                    if data:
+                        pw.map_reduce(my_func,data,my_reduce_function,reducer_wait_local=False)
+                else:
+                    queueLock.release()
+            print "Exiting " + self.name
+    queueLock = threading.Lock()
+    workQueue = Queue.Queue()
+    threadList = ["Thread-1", "Thread-2", "Thread-3"]
+    threads = []
+    threadID = 1
+    for tName in threadList:
+        thread = myThread(threadID, tName, workQueue)
+        thread.start()
+        threads.append(thread)
+        threadID += 1
+
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        fcntl.fcntl(s, fcntl.F_SETFL, os.O_NONBLOCK)
+        now = time.time()
+        batch=[]
+        while True:
+            try:
+                data = s.recv(4)
+            except socket.error as e:
+                err = e.args[0]
+                if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
+                    continue
+                else:
+                    print(e)
+                    sys.exit(1)
+            else:
+                if data:
+                    batch+=[int(data.decode())]
+                    print("Received:", repr(data))
+                else:
+                    print('meow')
+                    exitFlag = 1
+                    break
+                if time.time()-now > 2:
+                    print('batch',batch)
+                    queueLock.acquire()
+                    workQueue.put(batch)
+                    queueLock.release()
+                    batch=[]
+                    now=time.time()
+        s.close()
+        print('closed')
+    for t in threads:
+        t.join()
+    print(pw.get_result())
+
+
+#================== no thread ================
 
 def streamprocess(pw,my_func,my_reduce_function,connector='socket',host='localhost',port=65432,window=2):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -52,6 +110,7 @@ def streamprocess(pw,my_func,my_reduce_function,connector='socket',host='localho
                     now=time.time()
         s.close()
         print('closed')
+
     print(pw.get_result())
 
 
@@ -61,12 +120,9 @@ def streamprocess(pw,my_func,my_reduce_function,connector='socket',host='localho
 def my_func(x):
 	return x+10
 def my_reduce_function(results):
-    total = 0
-    for map_result in results:
-        total = total + map_result
-    return total
+    return sum(total)
 pw = pywren.ibm_cf_executor()
-streamprocess(pw,my_func,my_reduce_function,connector='socket',host='127.0.0.1',port=65432,window=2)
+streamprocess_threads(pw,my_func,my_reduce_function,connector='socket',host='127.0.0.1',port=65432,window=2)
 
 
 
