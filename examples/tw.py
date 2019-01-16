df61854dde02825a3a93c8a0c4b8d9b58e485000
Victor Chen <yvictorck@gmail.com>
2019-01-15 23:02:41 -0800

    a

---
+++ examples/tw.py
@@ -0,0 +1,7 @@
+import tweepy
+f = open("~/twkeys.txt", "r")
+consumer_key = f.readline()
+consumer_secret = f.readline()
+access_token = f.readline()
+access_token_secret = f.readline()
+print(consumer_key,consumer_secret,access_token,access_token_secret)
