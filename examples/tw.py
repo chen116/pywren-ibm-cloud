f8981f5d78b7169981681eb8fb13ddc72b9d5e3f
Victor Chen <yvictorck@gmail.com>
2019-01-15 23:07:54 -0800

    a

--- examples/tw.py
+++ examples/tw.py
@@ -5,3 +5,12 @@ consumer_secret = f.readline().strip()
 access_token = f.readline().strip()
 access_token_secret = f.readline().strip()
 print(consumer_key,consumer_secret,access_token,access_token_secret)
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print tweet.text
